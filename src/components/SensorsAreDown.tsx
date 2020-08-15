9eda53e66d8d2496a9c6cd26084bdd8d0994b482
Will Colton <coltonw@gmail.com>
2020-08-15 11:23:09 -0400

    :speaker: Add Alexa audio clip

--- src/components/SensorsAreDown.tsx
+++ src/components/SensorsAreDown.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FaAws } from 'react-icons/fa';
 import SectionHeading from './SectionHeading';
 import Link from './Link';
+import sensorsaredownAudio from '../media/sensorsaredown.mp3';
 import { ReactComponent as AlexaLogo } from '../media/alexa-logo.svg';
 import { ReactComponent as ServerlessLogo } from '../media/serverless-logo.svg';
 import { ReactComponent as FlowLogo } from '../media/flow-logo.svg';
@@ -29,6 +30,8 @@ const SensorsAreDown = () => {
         current state. I think I had some good ideas and I have follow-up ideas
         for redesigns but I shelved the game for now.
       </div>
+      <audio src={sensorsaredownAudio} controls className="w-full" />
+      <div className="text-xs text-center">Sorry for poor audio quality</div>
       <div className="my-4">
         This project involved mostly learning various AWS technologies that go
         into making an Alexa skill. I used an AWS Lambda which I deployed to
